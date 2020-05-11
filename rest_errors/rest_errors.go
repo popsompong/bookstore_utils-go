61a255921692c1b859045aa68e08d01e5ffa08a1
Sompong <ce16.engineering@gmail.com>
2020-05-12 00:00:27 +0700

    initial version

---
+++ rest_errors/rest_errors.go
@@ -0,0 +1,47 @@
+package rest_errors
+
+import (
+	"errors"
+	"net/http"
+)
+
+type RestErr struct {
+	Message string        `json:"message"`
+	Status  int           `json:"status"`
+	Error   string        `json:"error"`
+	Causes  []interface{} `json:"causes"`
+}
+
+func NewError(msg string) error {
+	return errors.New(msg)
+}
+
+func NewBadRequestError(message string) *RestErr {
+	return &RestErr{
+		Message: message,
+		Status:  http.StatusBadRequest,
+		Error:   http.StatusText(http.StatusBadRequest),
+	}
+}
+
+func NewNotFoundError(message string) *RestErr {
+	return &RestErr{
+		Message: message,
+		Status:  http.StatusNotFound,
+		Error:   http.StatusText(http.StatusNotFound),
+	}
+}
+
+func NewInternalServerError(message string, err error) *RestErr {
+	result := &RestErr{
+		Message: message,
+		Status:  http.StatusInternalServerError,
+		Error:   http.StatusText(http.StatusInternalServerError),
+		//Causes: []interface{}{err},
+	}
+	if err != nil {
+		result.Causes = append(result.Causes, err.Error())
+	}
+
+	return result
+}
---
+++ rest_errors/rest_errors_test.go
@@ -0,0 +1,36 @@
+package rest_errors
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"net/http"
+	"testing"
+)
+
+func TestNewInternalServerError(t *testing.T) {
+	err := NewInternalServerError("this is the message", errors.New("database error"))
+	assert.NotNil(t, err)
+	assert.EqualValues(t, http.StatusInternalServerError, err.Status)
+	assert.EqualValues(t, "this is the message", err.Message)
+	assert.EqualValues(t, http.StatusText(http.StatusInternalServerError), err.Error)
+	assert.NotNil(t, err.Causes)
+	assert.EqualValues(t, 1, len(err.Causes))
+	assert.EqualValues(t, "database error", err.Causes[0])
+
+	errBytes, _ := json.Marshal(err)
+	fmt.Println(string(errBytes))
+}
+
+func TestNewBadRequestError(t *testing.T) {
+	//TODO: Test!
+}
+
+func TestNewNotFoundError(t *testing.T) {
+	//TODO: Test!
+}
+
+func TestNewError(t *testing.T) {
+	//TODO: Test!
+}
